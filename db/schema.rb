ab8f8c773cddde7202764f34b97e79bf6f991f03
Andre Rodrigues <andre.rodrigues@tagview.com.br>
2015-08-21 23:01:33 -0300

    Added and setup pt-BR locales

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822011630) do
+ActiveRecord::Schema.define(version: 20150822013602) do
 
   create_table "interested_users", force: :cascade do |t|
     t.string   "name",       limit: 255
