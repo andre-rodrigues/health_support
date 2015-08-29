80c0b1b80ff2a413ac3c9b661db24252e56e1c40
Andre Rodrigues <andre.rodrigues@tagview.com.br>
2015-08-28 23:49:35 -0300

    Configurado botão de logout

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Health Support"
+  config.site_title = "HealthSupport"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -205,12 +205,11 @@ ActiveAdmin.setup do |config|
   #
   # To change the default utility navigation to show a link to your website & a logout btn
   #
-  #   config.namespace :admin do |admin|
-  #     admin.build_menu :utility_navigation do |menu|
-  #       menu.add label: "My Great Website", url: "http://www.mygreatwebsite.com", html_options: { target: :blank }
-  #       admin.add_logout_button_to_menu menu
-  #     end
-  #   end
+  config.namespace :admin do |admin|
+    admin.build_menu :utility_navigation do |menu|
+      menu.add label: "Sair", url: "/admin/logout", html_options: { method: :delete }
+    end
+  end
   #
   # If you wanted to add a static menu item to the default menu provided:
   #
